53910fd4617578260d9cefdda21c9e51327c8347
it4 <it4@it4.nowhere>
2018-10-23 23:13:48 +0200

    PEP8

--- python/git/getB2repos.py
+++ python/git/getB2repos.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Simple script to import all repos from 
+# Simple script to import all repos from
 
 import argparse
 
@@ -10,15 +10,15 @@ from os import system, mkdir, path
 
 def cloneRepo(repo_url, repo_owner_name, repos_dir):
     """ Clone repo_url repo and place it in repos_dir/repo_owner_name
-    Returns two arrays which respectively contains : 
+    Returns two arrays which respectively contains :
       - user : url of successfully cloned repos
       - user : url of faulty repos
     """
 
     # Building the git command
-    git_clone_command = ("git clone " + repo_url + " " 
-                   + repos_dir + "/" + repo_owner_name 
-                   + " &> /dev/null")
+    git_clone_command = ("git clone " + repo_url + " "
+                         + repos_dir + "/" + repo_owner_name
+                         + " &> /dev/null")
 
     stdout.write("\nCloning " + repo_owner_name + "'s repo")
 
@@ -26,13 +26,13 @@ def cloneRepo(repo_url, repo_owner_name, repos_dir):
         # Execute git clone and get return in a var
         command_return = system(git_clone_command)
 
-        # If git command has return 128 : unable to find repo 
+        # If git command has return 128 : unable to find repo
         # (private repo ? must manually accept invite)
         if command_return == 32768:
             stderr.write("\nCan't clone " + repo_url)
             return False
 
-        stdout.write("\n" + repo_url + " has been cloned.") 
+        stdout.write("\n" + repo_url + " has been cloned.")
 
         return True
 
@@ -42,13 +42,16 @@ def cloneRepo(repo_url, repo_owner_name, repos_dir):
 
         return False
 
+
 def pullRepo(repo_url, local_repo_path):
     """ Issue a simple 'git pull' from a specific directory
     The directory is repos_dir/repo_owner_name
     """
 
     try:
-        git_pull_command = "git --git-dir=" + local_repo_path + "/.git pull &> /dev/null"
+        git_pull_command = ("git --git-dir="
+                            + local_repo_path
+                            + "/.git pull &> /dev/null")
 
         system(git_pull_command)
 
@@ -58,7 +61,8 @@ def pullRepo(repo_url, local_repo_path):
 
     except Exception as e:
         stderr.write((str(e)))
-        stderr.write("\nCan't pull in " + local_repo_path + " from " + repo_url)
+        stderr.write("\nCan't pull in " + local_repo_path
+                     + " from " + repo_url)
 
         return False
 
@@ -66,7 +70,7 @@ def pullRepo(repo_url, local_repo_path):
 # GLOBAL VARS
 
 github_urls_file = "github_urls_file"
-repos_dir="./repos"
+repos_dir = "./repos"
 
 # will be formatted as name:git_connection_string
 all_repos = {}
@@ -74,7 +78,7 @@ all_repos = {}
 successfully_cloned_repos_owners = []
 faulty_repo_owners = []
 updated_repo_owners = []
-nawak_repo_owners = [] # Will store the repos whose name or content are non as asked
+nawak_repo_owners = []  # Owners of repo != "B2-Python"
 
 # CODE
 
@@ -111,21 +115,28 @@ with open(github_urls_file, "r") as file:
         repo_name = repo_git_url.split('/')[-1]
         if repo_name != "B2-Python":
             nawak_repo_owners.append(repo_owner_name)
-            stderr.write("\nRepo " + repo_name + " of " + repo_owner_name + " doesn't follow the name rule.")
+            stderr.write("\nRepo " + repo_name
+                         + " of " + repo_owner_name
+                         + " doesn't follow the name rule.")
             continue
 
-
-        # Test if the destination dir exists, if yes, pull the repo, otherwise, clone it.
+        # Test if the destination dir exists
+        # if yes, pull the repo
+        # otherwise, clone it.
         if path.isdir(local_repo_path):
-            stdout.write("\nThe repo of " + repo_owner_name + " has already been cloned. Pulling it.")
+            stdout.write("\nThe repo of " + repo_owner_name
+                         + " has already been cloned. Pulling it.")
+
             if pullRepo(repo_git_url, local_repo_path) is True:
                 updated_repo_owners.append(repo_owner_name)
+
             else:
                 faulty_repo_owners.append(repo_owner_name)
 
         else:
             if cloneRepo(repo_git_url, repo_owner_name, repos_dir) is True:
                 successfully_cloned_repos_owners.append(repo_owner_name)
+
             else:
                 faulty_repo_owners.append(repo_owner_name)
 
@@ -135,13 +146,14 @@ with open(github_urls_file, "r") as file:
 stdout.write("\nGet " + str(len(all_repos)) + " repos from file :")
 
 stdout.write("\n  " + str(len(faulty_repo_owners)) + " were faulty"
-             + "\n  " + str(len(successfully_cloned_repos_owners)) + " were cloned"
+             + "\n  " + str(len(successfully_cloned_repos_owners))
+             + " were cloned"
              + "\n  " + str(len(updated_repo_owners)) + " were pulled"
              + "\n  " + str(len(nawak_repo_owners)) + " nawak.")
 
 if len(faulty_repo_owners) > 0:
     stdout.write("\n\nFaulty repos (unable to clone) : ")
-    for name in faulty_repo_owners: 
+    for name in faulty_repo_owners:
         stdout.write("\n > " + name + " : " + all_repos[name])
 
 if len(nawak_repo_owners) > 0:
